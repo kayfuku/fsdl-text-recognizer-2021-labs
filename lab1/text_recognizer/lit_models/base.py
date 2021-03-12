2e07e3780fd19424c57b732f762eb04dd15df31a
Kei Fukutani <kifinewera@gmail.com>
2021-03-11 16:40:36 -0800

    set up local env

--- lab1/text_recognizer/lit_models/base.py
+++ lab1/text_recognizer/lit_models/base.py
@@ -29,7 +29,8 @@ class BaseLitModel(pl.LightningModule):
             self.loss_fn = getattr(torch.nn.functional, loss)
 
         self.one_cycle_max_lr = self.args.get("one_cycle_max_lr", None)
-        self.one_cycle_total_steps = self.args.get("one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS)
+        self.one_cycle_total_steps = self.args.get(
+            "one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS)
 
         self.train_acc = pl.metrics.Accuracy()
         self.val_acc = pl.metrics.Accuracy()
@@ -37,18 +38,25 @@ class BaseLitModel(pl.LightningModule):
 
     @staticmethod
     def add_to_argparse(parser):
-        parser.add_argument("--optimizer", type=str, default=OPTIMIZER, help="optimizer class from torch.optim")
+        parser.add_argument(
+            "--optimizer", type=str, default=OPTIMIZER,
+            help="optimizer class from torch.optim")
         parser.add_argument("--lr", type=float, default=LR)
         parser.add_argument("--one_cycle_max_lr", type=float, default=None)
-        parser.add_argument("--one_cycle_total_steps", type=int, default=ONE_CYCLE_TOTAL_STEPS)
-        parser.add_argument("--loss", type=str, default=LOSS, help="loss function from torch.nn.functional")
+        parser.add_argument("--one_cycle_total_steps",
+                            type=int, default=ONE_CYCLE_TOTAL_STEPS)
+        parser.add_argument(
+            "--loss", type=str, default=LOSS,
+            help="loss function from torch.nn.functional")
         return parser
 
     def configure_optimizers(self):
         optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
         if self.one_cycle_max_lr is None:
             return optimizer
-        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer=optimizer, max_lr=self.one_cycle_max_lr, total_steps=self.one_cycle_total_steps)
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer=optimizer, max_lr=self.one_cycle_max_lr,
+            total_steps=self.one_cycle_total_steps)
         return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
 
     def forward(self, x):
@@ -69,7 +77,8 @@ class BaseLitModel(pl.LightningModule):
         loss = self.loss_fn(logits, y)
         self.log("val_loss", loss, prog_bar=True)
         self.val_acc(logits, y)
-        self.log("val_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val_acc", self.val_acc, on_step=False,
+                 on_epoch=True, prog_bar=True)
 
     def test_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         x, y = batch
